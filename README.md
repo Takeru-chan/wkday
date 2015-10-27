0c6f14866dd2e065febdcb7dd76f67b5349e185f
Takeru-chan <takeru.katayama@gmail.com>
2015-10-27 21:43:34 +0900

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# wkday
+# Work day counter for PowerShell.
+稼働日を数えるPowerShellスクリプトです。おまけで暦日も数えます。  
+
+## Usage|使い方
+引数に正の整数を指定すると、今日を起点に指定日数後の日付を表示します。
+休日設定をすることで、指定稼働日数後の日付がわかります。  
+
+環境変数XCALWEEKに指定した曜日を休日と判定し、稼働日計数対象から外します。  
+日曜：0, 月曜：1, 火曜：2, ... 土曜：6
+土日休みの例）XCALWEEK=0,6
+
+引数なしで起動するとクレジット表示をします。  
+
+休日指定曜日に対し個別にこれをキャンセルし、稼働日扱いにすることができます。
+カレントディレクトリ中のworkday.lstファイルに指定された日付は、休日指定曜日であっても稼働日計数対象になります。  
+
+また休日指定曜日以外の祝日はカレントディレクトリ中のholiday.lstファイルで日付指定することで、稼働日計数対象から外すことができます。  
+
+workday.lst / holiday.lstともに日付指定は１行あたり１日づつyyyymmdd形式で記載します。  
+
+workday.lstで10月31日の土曜日を稼働日指定する例  
+20151031  
+
+holiday.lstで11月3日の火曜日を祝日指定する例  
+20151103  
+
+```
+(*'-') >> Get-Date
+
+2015年10月27日 21:30:42
+
+
+(*'-') >> [System.Environment]::GetEnvironmentVariable("XCALWEEK")
+0,6
+(*'-') >> .\wkday.ps1 14
+After 14 days is 2015/11/10(火) (calendar)
+After 14 days is 2015/11/16(月) (work day)
+```
+
+10月27日の14日後は暦日で11月10日であるが、土日を挟んでいるためこれを稼働日から外している。
+ただし10月31日を稼働日設定としているため、実際に稼働日計数対象外は11月の1日(日),3日(火),7日(土),8日(日)の4日間となり、14稼働日後は11月16日となった。  
+
+## License
+This script has released under the MIT license.
+[http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)
---
+++ wkday.ps1
@@ -0,0 +1,56 @@
+﻿param($n)
+#-----------------------------------------------------------------------
+$credit = @"
+  wkday.ps1 ver.0.1  2015.10.27  (c)Takeru.
+
+  Usage: wkday.ps1 n
+
+  Description:
+         The wkday utility counts calendar days and work days.
+         If arguments are not specified, help is displayed.
+
+         The week of day off is specified by the environment variable,
+         as XCALWEEK.
+         If you want to cancel the individual date, you may specify
+         by the file, named as "weekday.lst".
+         And you want to specify the individual holiday, you may specify
+         by the file, named as "holiday.lst".
+
+         Copyright (c) 2015 Takeru.
+         Release under the MIT license
+         http://opensource.org/licenses/MIT
+
+"@
+#-----------------------------------------------------------------------
+if (-Not ($n)) {
+  $credit
+  return
+}
+function wkday($day) {
+  $today = Get-Date
+  $hol = 0
+  $week = @("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  $dayoff = [System.Environment]::GetEnvironmentVariable("XCALWEEK").Split(",")
+  if (Test-Path workday.lst) {$workday = (Get-Content workday.lst) -as [String[]]}
+  if (Test-Path holiday.lst) {$holiday = (Get-Content holiday.lst) -as [String[]]}
+  :loop foreach ($n in 0..($day * 2)) {
+    $chkstr = $today.AddDays($n).toString("yyyyMMdd")
+    foreach ($dow in $dayoff) {
+      if ($week[$dow] -eq $today.AddDays($n).DayofWeek) {
+        $hol++
+      }
+    }
+    foreach ($str in $workday) {
+      if ($chkstr -eq $str) {$hol--}
+    }
+    foreach ($str in $holiday) {
+      if ($chkstr -eq $str) {$hol++}
+    }
+    if ($day -eq ($n - $hol)) {
+      break loop
+    }
+  }
+  Write-Host "After $day days is" $today.AddDays($day).toString("yyyy/MM/dd(ddd)") "(calendar)"
+  Write-Host "After $day days is" $today.AddDays($day + $hol).toString("yyyy/MM/dd(ddd)") "(work day)"
+}
+wkday $n
